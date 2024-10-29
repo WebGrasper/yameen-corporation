263067564b1f8c9be24c0ba03d278e249da9f06d
WebGrasper <mohdamaan570@gmail.com>
2024-10-29 18:54:04 +0000

    Containers: Added image-slider

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,11 +1,16 @@
-import ImageCarousal from "@/containers/image-carousal/image-carousal";
+
+import ImageSlider from "@/containers/image-slider/image-slider";
 import styles from "./page.module.css";
 
 export default function Product() {
   return (
     <main className={styles.root}>
       <div className={styles.main}>
-        <ImageCarousal />
+        <div className={styles.productGrid}>
+          <div className={styles.sliderContainer}><ImageSlider /></div>
+          <div className={styles.productInfoContainer}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eius autem recusandae quibusdam non laboriosam ipsa vel blanditiis nisi eligendi obcaecati, commodi praesentium repellendus voluptatum, porro mollitia tempore ad reprehenderit.</div>
+          <div className={styles.productDescriptionContainer}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium distinctio non esse a iure culpa. Animi rerum eveniet id, nihil cumque nam placeat. Neque nulla porro cumque saepe vero natus.</div>
+        </div>
       </div>
     </main>
   );
---
+++ containers/image-slider/image-slider.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useEffect, useState } from 'react';
+import styles from './image-slider.module.css';
+
+const ImageSlider= () =>{
+    const [counter, setCounter] = useState<number>(0);
+    const [activeFullImage, setActiveFullImage] = useState<boolean>(false);
+
+    const images = [
+        "https://ik.imagekit.io/ancorporation/Test%20resources/brass-camel.webp?updatedAt=1728314126048",
+        "https://ik.imagekit.io/ancorporation/Test%20resources/brass-bowl.webp?updatedAt=1728314125738",
+        "https://ik.imagekit.io/ancorporation/Test%20resources/brass-tortoise-urli.webp?updatedAt=1728314125956",
+      ];
+
+    useEffect(()=>{
+        const slides = document.querySelectorAll(`.${styles.slide}`);
+        slides.forEach((slide, index) => {
+            (slide as HTMLElement).style.left = `${index * 100}%`;
+        });
+    },[]);
+
+    useEffect(()=>{
+        (()=>{
+            const slides = document.querySelectorAll(`.${styles.slide}`);
+            slides.forEach(
+                (slide, index)=>{
+                    (slide as HTMLElement).style.transform = `translateX(-${counter * 100}%)`;
+                }
+            )
+        })();
+    },[counter]);
+
+    return(
+        <div className={`${styles.root} ${activeFullImage ? styles.rootActive : ''}`}>
+            {activeFullImage && <img src="/close-dark.png" alt="close button" className={styles.closeButton} onClick={()=> setActiveFullImage(false)}/>}
+            {images.map((src, index)=>(
+                <img
+                  src={src}
+                  alt=""
+                  loading="lazy"
+                  key={index}
+                  className={`${styles.slide} ${activeFullImage ? styles.slideActive : ''}`}
+                  onClick={()=> setActiveFullImage(true)}
+                />
+            ))}
+            <img src="/left-arrow-filled-dark.png" alt="left button" className={`${styles.leftButton} ${activeFullImage ? styles.leftButtonActive : ''}`} onClick={()=> setCounter(prev => prev - 1)}/>
+            <img src="/right-arrow-filled-dark.png" alt="right button" className={`${styles.rightButton} ${activeFullImage ? styles.rightButtonActive : ''}`} onClick={()=> setCounter(prev => prev + 1)}/>
+        </div>
+    );
+}
+
+export default ImageSlider;
